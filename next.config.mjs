257730ea7d3099d7de93822c72e1abbea907b0de
John Michael Panganiban <johnmichaelpanganiban.its@gmail.com>
2024-10-24 21:36:24 +0800

    initiate ci/cd

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    // basePath: '/finance-landing-page',
-    // assetPrefix: '/finance-landing-page/',
-    output: 'export',
-};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     // basePath: '/finance-landing-page',
+//     // assetPrefix: '/finance-landing-page/',
+//     output: 'export',
+// };
 
-export default nextConfig;
+// export default nextConfig;
+module.exports = {
+    assetPrefix: '/clearpath/',
+    basePath: '/clearpath',
+};
+  
