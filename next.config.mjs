a74c9b03c2a5d17b6ca39a0f8efad9a624bcc128
John Michael Panganiban <johnmichaelpanganiban.its@gmail.com>
2024-10-30 08:45:10 +0800

    removed assetPrefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: '/clearpath',
-    assetPrefix: '/clearpath/',
+    // assetPrefix: '/clearpath/',
     output: 'export',
 };
 
